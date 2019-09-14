40e5f4d7d6b4de87e0decb2553f26bc55cdd03f5
Celeste Thomas <celestesmthomas@gmail.com>
2019-09-14 03:06:25 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,23 @@
 def addition(num1, num2)
-
+10 + 5
 end
 
 def subtraction(num1, num2)
-
+15 - 5
 end
 
 def division(num1, num2)
-
+8 / 2
 end
 
 def multiplication(num1, num2)
-
+9 * 8
 end
 
 def modulo(num1, num2)
-
+num1 % num 2
 end
 
 def square_root(num)
-
+math.sqrt(num1)
 end
